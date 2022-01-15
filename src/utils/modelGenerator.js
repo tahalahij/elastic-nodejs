c4b0704c076cfeb05ac187ea2c345f0925948492
tahalahij <t.ghasemi@emofid.com>
2022-01-15 20:40:14 +0330

    enhance: add init models to modelGenerator to add models to sequelize

--- src/utils/modelGenerator.js
+++ src/utils/modelGenerator.js
@@ -4,10 +4,17 @@ import { Mysql } from "../connections";
 const generator = new SequelizeAuto(Mysql, null, null, {
     dialect: 'mysql',
     directory: 'generatedModels',
-    // caseFile: 'l',
+    caseFile: 'l', // lowerCase model names
     // caseModel: 'p',
     // caseProp: 'c',
     language: 'es6',
     singularize: false,
 });
+// reads the model files generated in generatedModels folder and adds them to Sequelize,
+// this functions returns models but models can be retrieved using Mysql.models too
+export const initModels = async () => {
+    const root = process.cwd();
+    const { default: init } = await import(`${root}/generatedModels/init-models`);
+    return init(Mysql);
+}
 export default generator
