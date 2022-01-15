3dad29fb8ea7a9f5a8752a272d59563c683cdb36
tahalahij <t.ghasemi@emofid.com>
2022-01-15 22:59:23 +0330

    enhance: remove thesis related files to use the general index routes

--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,3 @@
-import { Thesis } from './thesis.model.sql';
 import Index  from './Index.model.mongodb';
 
-export { Thesis , Index };
+export { Index };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,11 @@
 import { Router } from 'express';
-import thesisRoutes from './thesis.routes';
 import tablesRoutes from './tables.routes';
 import { startTriggerQueue } from "../queues";
-import { ElasticSearch } from "../connections";
 
 const router = Router();
 
-router.get('/', (req, res) => res.render('search', { baseUrl: 'http://localhost:3001/' }));
+// router.get('/', (req, res) => res.render('search', { baseUrl: 'http://localhost:3001/' }));
+router.get('/', (req, res) => res.send(" app is running, You need to open the panel "));
 router.get('/api/v1/startrabbit', async (req, res) => {
     try {
         await startTriggerQueue()
@@ -16,19 +15,8 @@ router.get('/api/v1/startrabbit', async (req, res) => {
     }
 })
 router.use(
-        '/api/v1/thesis',
-        thesisRoutes,
-);
-router.use(
         '/api/v1/tables',
         tablesRoutes,
-);
-router.use(
-        '/test',
-        async (req, res) => {
-            const x = await ElasticSearch.getIndex({ index: 'article' })
-            res.send({ x })
-        },
 );
 
 export default router;
